8aa020b6b8c3d774d55f05d6c9fe7dd15b53689d
hlmurph <106940356+hlmurph@users.noreply.github.com>
2025-04-02 07:45:32 -0400

    Add solutions to exercises 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result = '';
+    strArray = [];
+    for (i = str.length; i >= 0; i--) {
+        strArray.push(str.charAt(i))
+    }
+    for (letter of strArray) {
+        result += letter;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let newArray = [];
+    let toRemove = [];
+    for (let i = 1; i < arguments.length; i++) {
+        toRemove.push(arguments[i]);
+    }
+    outer: for (const item of array) {
+        for (const removeItem of toRemove) {
+            if (item === removeItem) {
+                continue outer;
+            }
+        }
+        newArray.push(item);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
